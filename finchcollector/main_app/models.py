c2e472938459a52159e426d109dcfa33906da434
miya-w <62531644+hellomiyawu@users.noreply.github.com>
2023-08-20 12:36:51 +1000

    update part-5

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -7,12 +7,25 @@ MEALS = (
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+# Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     size = models.CharField(max_length=100)
     habitation = models.TextField(max_length=300)
+      # Add the M:M relationship
+    toys = models.ManyToManyField(Toy)
 
   # new code below
     def __str__(self):
